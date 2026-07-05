b45cbd786625282c1c39e06bf7d5bf95a6df2811
Daniel Miller <danielmiller@CHMAC02-2.local>
2026-07-05 16:25:17 -0500

    v3.1.19: Fix MK4 DAW Mode Init Message

--- src/HardwareIntegration.h
+++ src/HardwareIntegration.h
@@ -24,16 +24,15 @@ extern int gOutPort;
 inline void sendLaunchkeyDawModeInit(int client, int port) {
     if (gSeqOut == nullptr || gOutPort < 0) return;
     
-    // MK4 DAW mode SysEx: F0 00 20 29 02 13 0C 7F F7
-    unsigned char mk4_sysex[] = {0xF0, 0x00, 0x20, 0x29, 0x02, 0x13, 0x0C, 0x7F, 0xF7};
-    
+    // MK4 DAW mode enable: Note On Ch 16, Note 12, Velocity 127 (9F 0C 7F)
     snd_seq_event_t ev;
     snd_seq_ev_clear(&ev);
     snd_seq_ev_set_source(&ev, gOutPort);
     snd_seq_ev_set_dest(&ev, client, port);
     snd_seq_ev_set_direct(&ev);
     
-    snd_seq_ev_set_sysex(&ev, sizeof(mk4_sysex), mk4_sysex);
+    // Note On (Channel 15 is 0-indexed Channel 16)
+    snd_seq_ev_set_noteon(&ev, 15, 12, 127);
     snd_seq_event_output_direct(gSeqOut, &ev);
     
     std::cout << "HardwareIntegration: Sent DAW Mode Init to Launchkey " << client << ":" << port << std::endl;
